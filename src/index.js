601ac691def978ce31905439c1735ad7dc575211
Ilya Pavlov <spirkaa@gmail.com>
2016-07-18 21:27:52 +0300

    preparations for production build; move to webpack 2 single file env conditional config

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
-import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import { browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
+import { AppContainer } from 'react-hot-loader'
 import Root from './main/containers/Root'
 import configureStore from './store'
 
-import '../node_modules/bootswatch/yeti/bootstrap.min.css'
+import './assets/css/bootstrap.min.css'
 import '../node_modules/react-bootstrap-table/css/react-bootstrap-table-all.min.css'
-import './main/components/style.css'
+import './assets/css/style.css'
 
 const store = configureStore()
 const history = syncHistoryWithStore(browserHistory, store)
+const container = document.getElementById('root')
 
 render(
-  <Root store={store} history={history}/>,
-  document.getElementById('root')
+  <AppContainer>
+    <Root store={store} history={history}/>
+  </AppContainer>,
+  container
 )
+
+if (module.hot) {
+  module.hot.accept('./main/containers/Root', () => {
+    const NextApp = require('./main/containers/Root').default
+    render(
+      <AppContainer>
+        <Root store={store} history={history}/>
+      </AppContainer>,
+      container
+    )
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,128 @@
-var path = require('path')
-var webpack = require('webpack')
-var autoprefixer = require('autoprefixer')
-var precss = require('precss')
+const { resolve } = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+const autoprefixer = require('autoprefixer')
+const precss = require('precss')
 
-module.exports = {
-  devtool: 'cheap-module-eval-source-map',
-  entry: [
-    'webpack-hot-middleware/client',
-    'babel-polyfill',
-    './src/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  module: {
-    preLoaders: [
-      {
-        test: /\.js$/,
-        loaders: [ 'eslint' ],
-        include: [
-          path.resolve(__dirname, 'src')
-        ]
-      }
-    ],
-    loaders: [
-      {
-        loaders: [ 'react-hot', 'babel-loader' ],
-        include: [
-          path.resolve(__dirname, 'src')
-        ],
-        test: /\.js$/,
-        plugins: [ 'transform-runtime' ]
-      },
-      { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
+const postcss = () => ([ autoprefixer, precss ])
 
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader!postcss-loader'
-      },
-      {
-        test: /\.svg/,
-        loader: 'svg-url-loader'
-      }
-    ]
-  },
-  postcss: function () {
-    return [ autoprefixer, precss ]
+const SOURCE = resolve(__dirname, 'src')
+
+module.exports = (env = {}) => {
+  const addItem = (add, item) => add ? item : undefined
+  const ifProd = item => addItem(env.prod, item)
+  const ifDev = item => addItem(!env.prod, item)
+  const removeEmpty = array => array.filter(i => !!i)
+
+  return {
+    entry: {
+      app: removeEmpty([
+        ifDev('react-hot-loader/patch'),
+        ifDev('webpack-dev-server/client?http://localhost:3000'),
+        ifDev('webpack/hot/only-dev-server'),
+        'babel-polyfill',
+        './index'
+      ])
+    },
+    output: {
+      filename: env.prod ? '[name].[hash].js' : '[name].js',
+      chunkFilename: '[name].[chunkhash].js',
+      path: resolve(__dirname, 'dist'),
+      pathinfo: !env.prod,
+      publicPath: ''
+    },
+    context: SOURCE,
+    devtool: env.prod ? 'hidden-source-map' : 'cheap-module-eval-source-map',
+    bail: env.prod,
+    resolve: {
+      extensions: ['', '.js', '.jsx']
+    },
+    module: {
+      preLoaders: [
+        {
+          test: /\.jsx?$/,
+          loader: 'eslint',
+          include: [ SOURCE ]
+        }
+      ],
+      loaders: removeEmpty([
+        {
+          test: /\.jsx?$/,
+          loader: 'babel',
+          include: [ SOURCE ],
+          query: {
+            'presets': [ 'es2015-webpack', 'react', 'stage-0' ],
+            'plugins': removeEmpty([
+              ifDev('react-hot-loader/babel'),
+              'transform-runtime' ])
+          }
+        },
+        ifProd({
+          test: /\.[l|s]?[a|e|c]ss$/,
+          loader: ExtractTextPlugin.extract({
+            notExtractLoader: 'style',
+            loader: 'css!postcss'
+          })
+        }),
+        ifDev({
+          test: /\.[l|s]?[a|e|c]ss$/,
+          loaders: [ 'style', 'css', 'postcss' ]
+        }),
+        {
+          test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'url?limit=10000&mimetype=application/font-woff'
+        },
+        {
+          test: /\.(ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'file'
+        },
+        {
+          test: /\.(png|gif|jpg)(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'file'
+        }
+      ])
+    },
+    plugins: removeEmpty([
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': env.prod ? '"production"' : '"development"'
+        }
+      }),
+
+      ifDev(new webpack.HotModuleReplacementPlugin()),
+      ifDev(new webpack.NoErrorsPlugin()),
+
+      ifProd(new webpack.optimize.DedupePlugin()),
+      ifProd(new webpack.optimize.OccurrenceOrderPlugin()),
+      ifProd(new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false,
+        quiet: true
+      })),
+      ifProd(new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          screw_ie8: true,
+          warnings: false
+        }
+      })),
+      ifProd(new ExtractTextPlugin({
+        filename: '[name].[chunkhash].css',
+        allChunks: true
+      })),
+      ifProd(new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './assets/index.template.html',
+        favicon: './assets/favicon.ico'
+      })),
+      ifProd(new CompressionPlugin({
+        asset: '[file].gz',
+        algorithm: 'gzip',
+        test: /\.js$|\.css$/,
+        threshold: 1100,
+        minRatio: 0.8
+      }))
+    ])
   }
 }
